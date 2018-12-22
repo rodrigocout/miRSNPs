f812624012eeba95911c0c5350e9e3451d016b7c
Rodrigo C de Almeida <rodrigocout@users.noreply.github.com>
2018-12-22 07:27:12 +0100

    Update intersect_eQTL.R

--- intersect_eQTL.R
+++ intersect_eQTL.R
@@ -2,13 +2,9 @@
 ###Blood eQTL####
 ##cis eQTL###
 cis_eqtl <- read.delim('~/Dropbox/BED_files_miRSNPS/databases/Blood_eQTL_CisAssociationsProbeLevelFDR0.5.txt', header = T)
-head(cis_eqtl)
-colnames(cis_eqtl)
-length(cis_eqtl$SNPName)
 
 load(file = "~/Dropbox/BED_files_miRSNPS/databases/navyResultsData.RData")
 colnames(navyD)
-length(navyD$SNP)
 
 #Intersect
 inter_eqtl <- intersect(cis_eqtl$SNPName,navyD$SNP)
@@ -20,9 +16,6 @@ save(inter_eqtl, file = "~/Dropbox/BED_files_miRSNPS/databases/inter_Blood_cis_e
 
 ###trans-eqtl#######
 trans_eqtl <- read.delim('~/Dropbox/BED_files_miRSNPS/databases/Blood_eQTL_TransEQTLsFDR0.5.txt', header = T)
-head(trans_eqtl)
-colnames(trans_eqtl)
-length(trans_eqtl$SNPName)
 
 #Intersect
 inter_trans_eqtl <- intersect(trans_eqtl$SNPName,navyD$SNP)
@@ -36,10 +29,8 @@ save(inter_eqtl, file = "~/Dropbox/BED_files_miRSNPS/databases/inter_Blood_trans
 gwas_ciseqtl <- intersect(inter_gwas$SNPs, inter_eqtl$SNPs_eqtl)
 length(gwas_ciseqtl)
 gwas_ciseqtl <- as.data.frame(gwas_ciseqtl)
-head(gwas_ciseqtl)
 save(gwas_ciseqtl, file = "~/Dropbox/BED_files_miRSNPS/databases/GWAS_Blood_cis_eQTL.RData")
 
 gwas_transeqtl <- intersect(inter_gwas$SNPs, inter_trans_eqtl$SNPs_trans_eqtl)
-length(gwas_transeqtl)
 gwas_transeqtl <- as.data.frame(gwas_transeqtl)
 save(gwas_transeqtl, file = "~/Dropbox/BED_files_miRSNPS/databases/GWAS_Blood_trans_eQTL.RData")
